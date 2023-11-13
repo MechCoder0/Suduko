39fa216ac8166515f68758dd3005ae84bbc1774b
MechCoder <engineer.jmeck@gmail.com>
2023-11-13 16:14:08 -0500

    Completed drawing the board

--- screen_writer.py
+++ screen_writer.py
@@ -45,6 +45,41 @@ class ScreenWriter:
         self.write_text((0, self.screen_height * .75), "Begin!", True, Util.get_middle(0, self.screen_width))
 
 
+
     def print_sudoku_board(self):
         self.surface.fill(self.WHITE)
-        pygame.draw.line(self.surface, self.BLACK, (100, 100), (700, 500), 5)
+
+        #Board is 300 x 300 px
+        top_left = (50, 50)
+        bottom_right = (950, 950)
+        distance_between = 100
+
+        self.draw_square(top_left, bottom_right)
+        self.draw_vertical_soduko(top_left[1], bottom_right[1], distance_between, top_left[0])
+        self.draw_horizontal_soduko(top_left[0], bottom_right[0], distance_between, top_left[1])
+        
+
+    def draw_horizontal_soduko(self, start, end, dist, start_y):
+        for x in range(9):
+            pygame.draw.line(self.surface, self.BLACK, (start, (dist * x) + start_y), 
+                             (end, dist * x + start_y), 5)
+
+    def draw_vertical_soduko(self, start, end, dist, start_x):
+        for x in range(9):
+            pygame.draw.line(self.surface, self.BLACK, ((dist * x) + start_x, start), 
+                             (dist * x + start_x, end), 5)
+
+
+    def draw_square(self, top_left, bottom_right):
+        top = top_left[1]
+        left = top_left[0]
+        bottom = bottom_right[1]
+        right = bottom_right[0]
+        top_left = (left,top)
+        top_right = (right, top)
+        bottom_left = (left, bottom)
+        bottom_right = (right, bottom)
+        pygame.draw.line(self.surface, self.BLACK, top_left, top_right, 5)
+        pygame.draw.line(self.surface, self.BLACK, top_left, bottom_left, 5)
+        pygame.draw.line(self.surface, self.BLACK, bottom_left, bottom_right, 5)
+        pygame.draw.line(self.surface, self.BLACK, bottom_right, top_right, 5)
