ebb19e5d62b49ca6eee096a5fb7146e2d48f517a
MechCoder <engineer.jmeck@gmail.com>
2026-03-14 20:39:04 -0400

    Added consistency

--- GameObjects/button.py
+++ GameObjects/button.py
@@ -1,8 +1,8 @@
 from .generic_sprite import GenericSprite
 
 class Button(GenericSprite):
-    def __init__(self, size, center, onclick, text, color=(255,0,0), groups=None):
+    def __init__(self, size, center, on_click, text, color=(255,0,0), groups=None):
         super().__init__(center, size, groups=groups, color=color, text=text)
         self.clicked = False
-        self.on_click = onclick
+        self.on_click = on_click
         
